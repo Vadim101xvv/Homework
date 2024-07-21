dd736b168a3233807358c46a8fe09fe57395f74d
Vadim101xvv <khramov.vadim@yandex.ru>
2024-07-21 16:03:09 +0300

    Дз "Средний балл"

---
+++ module1hard.py
@@ -0,0 +1,8 @@
+grades = [[3, 4, 5], [5, 5, 4, 5], [3, 2, 3]]
+students = {'Nadia', 'Valera', 'Sasha'}
+Nadia = sum(grades[0])/len(grades[0])
+Sasha = sum(grades[1])/len(grades[1])
+Valera = sum(grades[2])/len(grades[2])
+class_book = {'Nadia': Nadia, 'Sasha': Sasha, 'Valera': Valera}
+print(class_book)
+
